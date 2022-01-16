32d4ab61ef4b2f61c1058def8aeba1d0f3ffb6c8
ShawnZhang0828 <xiang.zhang0828@gmail.com>
2022-01-16 05:58:47 -0500

    add page navigations, add the contact section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const openMenu = document.querySelector("#show-menu");
 const openMenuPersonalInfo = document.querySelector("#info-container");
 const openMenuPersonalIntro = document.querySelector("#self-intro");
 const openMenuProject = document.querySelector("#project-container");
+const openMenuResume = document.querySelector("#resume-container")
+const openMenunContact = document.querySelector("#contact-container")
 const hideMenuIcon = document.querySelector("#hide-menu");
 const sideMenu = document.querySelector("#nav-menu");
 
@@ -10,6 +12,8 @@ openMenu.addEventListener("click", function() {
     openMenuPersonalInfo.classList.add("active")
     openMenuPersonalIntro.classList.add("active")
     openMenuProject.classList.add("active")
+    openMenuResume.classList.add("active")
+    openMenunContact.classList.add("active")
 })
 
 hideMenuIcon.addEventListener("click", function() {
@@ -17,5 +21,27 @@ hideMenuIcon.addEventListener("click", function() {
     openMenuPersonalInfo.classList.remove("active")
     openMenuPersonalIntro.classList.remove("active")
     openMenuProject.classList.remove("active")
+    openMenuResume.classList.remove("active")
+    openMenunContact.classList.remove("active")
 })
 
+// const about_button = document.getElementById("about-btn");
+// const skills_button = document.getElementById("skills-btn");
+// const projects_button = document.getElementById("projetcs-btn");
+// const resume_button = document.getElementById("resume-btn");
+// const contact_button = document.getElementById("contact-btn");
+// about_button.addEventListener("click", () => {
+//     document.getElementById("info-container").scrollIntoView({behavior: 'smooth'})
+// })
+// skills_button.addEventListener("click", () => {
+//     document.getElementById("project-container").scrollIntoView({behavior: 'smooth'})
+// })
+// projects_button.addEventListener("click", () => {
+//     document.getElementById("project-container").scrollIntoView({behavior: 'smooth'})
+// })
+// resume_button.addEventListener("click", () => {
+//     document.getElementById("resume-container").scrollIntoView({behavior: 'smooth'})
+// })
+// contact_button.addEventListener("click", () => {
+//     document.getElementById("contact-container").scrollIntoView({behavior: 'smooth'})
+// })
